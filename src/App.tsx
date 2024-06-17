a324ff976f1afd652497cb8c2aa95cbff1c72d16
unknown <matheus.duartesilva42@gmail.com>
2024-06-17 14:30:34 -0300

    feat: formulário básico

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { useForm } from 'react-hook-form'
 import './styles/global.css'
+import { useState } from 'react'
+
+/*
+  [] Validação / Transformação
+  [] Field Arrays
+  [] Upload de Arquivos
+  [] Composition Pattern
+*/
 
 const App = () => {
+  const [output, setOutput] = useState('')
+  const { register, handleSubmit } = useForm()
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2))
+  }
+
   return (
-    <main className="h-screen bg-zinc-50 flex items-center justify-center">
-      <form className='flex flex-col gap-4 w-full max-w-xs'>
+    <main className="h-screen bg-zinc-50 flex flex-col gap-10 items-center justify-center">
+      <form
+        className='flex flex-col gap-4 w-full max-w-xs'
+        onSubmit={handleSubmit(createUser)}
+      >
         <div className="flex flex-col gap-1">
           <label htmlFor="">Email</label>
           <input
             type="email"
-            name="email"
             className='border border-zinc-200 shadow-sm rounded h-10 px-3'
+            {...register('email')}
           />
         </div>
 
@@ -17,8 +36,8 @@ const App = () => {
           <label htmlFor="">Senha</label>
           <input
             type="password"
-            name="password"
             className='border border-zinc-200 shadow-sm rounded h-10 px-3'
+            {...register('password')}
           />
         </div>
 
@@ -29,6 +48,8 @@ const App = () => {
           Salvar
         </button>
       </form>
+
+      <pre>{output}</pre>
     </main>
   )
 }
