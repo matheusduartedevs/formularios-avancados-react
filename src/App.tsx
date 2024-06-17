58e1a4a6f9cd44074938b6ceb9bf949ad512f3ea
unknown <matheus.duartesilva42@gmail.com>
2024-06-17 15:10:39 -0300

    feat: implementação de validação e transformação nos formulários

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import { useForm } from 'react-hook-form'
 import './styles/global.css'
 import { useState } from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 /*
-  [] Validação / Transformação
+  [X] Validação / Transformação
   [] Field Arrays
   [] Upload de Arquivos
   [] Composition Pattern
 */
 
+const createUserFormSchema = z.object({
+  name: z.string()
+    .min(1, 'O nome é obrigatório')
+    .transform(name => {
+      return name.trim().split(' ').map(word => {
+        return word[0].toLocaleUpperCase().concat(word.substring(1))
+      }).join(' ')
+    }),
+
+  email: z.string()
+    .min(1, 'O e-mail é obrigatório')
+    .email('Formato de e-mail inválido')
+    .toLowerCase(),
+
+  password: z.string()
+    .min(6, 'A senha precisa de no mínimo 6 caracteres')
+})
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
+
 const App = () => {
   const [output, setOutput] = useState('')
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema)
+  })
 
   function createUser(data: any) {
     setOutput(JSON.stringify(data, null, 2))
@@ -24,21 +48,33 @@ const App = () => {
         onSubmit={handleSubmit(createUser)}
       >
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Email</label>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            className='border border-zinc-200 shadow-sm rounded h-10 px-3'
+            {...register('name')}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email">Email</label>
           <input
             type="email"
             className='border border-zinc-200 shadow-sm rounded h-10 px-3'
             {...register('email')}
           />
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
 
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Senha</label>
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
             className='border border-zinc-200 shadow-sm rounded h-10 px-3'
             {...register('password')}
           />
+          {errors.password && <span>{errors.password.message}</span>}
         </div>
 
         <button
